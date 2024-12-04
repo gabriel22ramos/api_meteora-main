ab7a65ada311fd91359351bd796035860bb55345
Gabriel Ramos <gabrielramoss232@gmail.com>
2024-12-04 16:34:59 -0300

    9

--- api_meteora-main/src/main/java/br/com/meteora/api/security/SecurityConfig.java
+++ api_meteora-main/src/main/java/br/com/meteora/api/security/SecurityConfig.java
@@ -2,11 +2,14 @@ package br.com.meteora.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -17,13 +20,33 @@ public class SecurityConfig {
         UserDetails usuario1 = User.builder()
         .username("joao@email.com")
         .password("{noop}joao123")
+        .roles("USER")
         .build();
 
         UserDetails usuario2 = User.builder()
         .username("maria@email.com")
         .password("{noop}maria123")
+        .roles("USER")
         .build();
 
         return new InMemoryUserDetailsManager(usuario1, usuario2);
     }
-}
+   
+    @Bean 
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{  
+        http
+            .authorizeHttpRequests(auth -> auth
+               .requestMatchers("/h2-console/**","/", "/css/**","/assets/**").permitAll()
+               .anyRequest().authenticated()
+               )
+
+
+               .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+               .headers(headers -> headers.frameOptions(Customizer.withDefaults()).disable());
+
+
+       return http.build();
+   }
+
+} 
+
